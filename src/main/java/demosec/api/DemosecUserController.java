c11a90636e36ed4bcedda7790b894fa626c12be6
WinsonHuang <hwangwc@qq.com>
2023-08-19 14:11:13 +0800

    Add a controller method for test

--- src/main/java/demosec/api/DemosecUserController.java
+++ src/main/java/demosec/api/DemosecUserController.java
@@ -1,9 +1,15 @@
 package demosec.api;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import demosec.data.DemosecUserRepo;
+import demosec.domain.DemosecUser;
 
 @RestController
 @RequestMapping(path = "/api/user")
@@ -13,5 +19,14 @@ public class DemosecUserController {
     @GetMapping(path = "/test")
     public String testMessage() {
         return "Controller Available.\n";
+    }
+
+
+    @Autowired
+    DemosecUserRepo demosecUserRepo;
+
+    @GetMapping
+    public List<DemosecUser> getAllUser() {
+        return demosecUserRepo.findAll();
     }
 }
