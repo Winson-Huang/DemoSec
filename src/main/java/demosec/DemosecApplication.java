9631eee3a7e9b0f3eaad8c24893b083c5c8b36cb
WinsonHuang <hwangwc@qq.com>
2023-08-19 12:12:00 +0800

    Create project

---
+++ src/main/java/demosec/DemosecApplication.java
@@ -0,0 +1,13 @@
+package demosec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemosecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemosecApplication.class, args);
+	}
+
+}
